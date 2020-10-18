5bdda0bdf9306489c85612954393c1880f7e4b3f
DizzyHurricane5001 <72887223+DizzyHurricane5001@users.noreply.github.com>
2020-10-18 23:44:01 +0100

    Create SQLdb.java

---
+++ SQLdb.java
@@ -0,0 +1,89 @@
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.sql.*;
+
+public class SQLdb {
+    private Connection connect = null;
+    private Statement statement = null;
+    private PreparedStatement preparedStatement = null;
+    private ResultSet resultSet = null;
+
+    private static JsonParser parser;
+    private static JsonElement jsonTree;
+    private static JsonObject jsonObject;
+    private static JsonElement rootObject;
+    private static JsonElement treeObject;
+
+    public void connectDB() throws Exception {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String password = "";
+            String address = "";
+            connect = DriverManager
+                    .getConnection("jdbc:mysql://"+address+"/sql2371581?&"
+                            + "user=sql2371581&password=" +password);
+
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+
+    public void insertData(String ofType, int serverNumber,  String insertData) throws SQLException {
+        parser = new JsonParser();
+        jsonTree = parser.parse(insertData);
+        jsonObject = jsonTree.getAsJsonObject();
+
+
+        switch(ofType) {
+            case "serverData":
+                resetServerData();
+                rootObject = jsonObject.get("server");
+                JsonObject treeObject = rootObject.getAsJsonObject();
+                //String dxp_status = treeObject.get("dxp").toString();
+
+                //JsonArray jarray = jsonObject.getAsJsonArray("server");
+                String uptime = treeObject.get("uptime").toString();
+                String dxp_status = treeObject.get("dxp").toString();
+                String serverRegion = treeObject.get("region").toString();
+
+
+            preparedStatement = connect
+                    .prepareStatement("insert into serverdata values (default, ?, ?, ?, ?,default,?)");
+            preparedStatement.setString(1, serverRegion + "" + serverNumber); // server id
+            preparedStatement.setString(2, dxp_status); // dxp status
+            preparedStatement.setString(3, uptime); // uptime
+            preparedStatement.setInt(4, 1); //dxp duration is status = true
+                preparedStatement.setInt(5, 0); //dxp expired true false
+            //    preparedStatement.setTimestamp(5, );
+                preparedStatement.executeUpdate(); //insert
+            break;
+        }
+    }
+    public void resetServerData() throws SQLException {
+        statement = connect.createStatement();
+        String query = "DROP TABLE serverdata";
+        PreparedStatement s = connect.prepareStatement(query);
+
+    }
+
+  public void close() {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+
+            if (statement != null) {
+                statement.close();
+            }
+
+            if (connect != null) {
+                connect.close();
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
+}
