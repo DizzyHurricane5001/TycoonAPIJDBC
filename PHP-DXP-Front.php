fedb04546e7429ae89dc49158cf9d2a887f5e608
DizzyHurricane5001 <72887223+DizzyHurricane5001@users.noreply.github.com>
2020-10-19 15:47:16 +0100

    Update PHP-DXP-Front.php

--- PHP-DXP-Front.php
+++ PHP-DXP-Front.php
@@ -1,22 +1,16 @@
 <?php
-//echo "<b> <center>You're viewing the current status of Tycoon DXP across all ten servers. This is live data. </b><br/><br/>";
 $servername = "";
-$username = ";
+$username = "";
 $password = "";
 $dbname = "";
-
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+if ($conn->connect_error)
+{
+    die("Connection failed: " . $conn->connect_error);
 }
-
 $sql = "SELECT * FROM serverdata";
 $result = $conn->query($sql);
-
 //require('index.html'); // HTML,CSS,JS ETC design code
-
 echo '<table class="table table-bordered">
   <thead>
     <tr>
@@ -26,33 +20,36 @@ echo '<table class="table table-bordered">
       <th scope="col">Server Info</th>
     </tr>
   </thead>';
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-if ($row['expired'] == 0) {
-  $dxp = $row['dxp'];
-  $dxp_array = explode (",", $dxp); 
-   if ($dxp_array[0] == "[true") {
-echo ' <tr>
-      <th scope="row">'.$row["serverid"].'</th>
-      <td>'.$dxp_array[0].'</td>
-      <td>'.floor($dxp_array[2]/60000).'</td>
-      <td>'.$dxp_array[1].'</td>
+if ($result->num_rows > 0)
+{
+    while ($row = $result->fetch_assoc())
+    {
+            $dxp = $row['dxp'];
+            $dxp_array = explode(",", $dxp);
+            if ($dxp_array[0] == "[true")
+            {
+                echo ' <tr>
+      <th scope="row">' . $row["serverid"] . '</th>
+      <td>' . $dxp_array[0] . '</td>
+      <td>' . floor($dxp_array[2] / 60000) . '</td>
+      <td>' . $dxp_array[1] . '</td>
     </tr>';
-   } else {
-echo '<tr>
-      <th scope="row">'.$row["serverid"].'</th>
+            }
+            else
+            {
+                echo '<tr>
+      <th scope="row">' . $row["serverid"] . '</th>
       <td><p style="color:red">No DXP Active.</p></td>
       <td> </td>
-      <td>'.str_replace('"', "", $row['uptime']).'</td>
+      <td>' . str_replace('"', "", $row['uptime']) . '</td>
     </tr>';
-  } 
-} else {
-
+        }
+    }
 }
-  }
-} else {
- // echo "0 results";
+else
+{
+    // echo "0 results";
+    
 }
 echo '</table>';
 echo "<b> This is live data. If a server does not show it is offline.</b>";
