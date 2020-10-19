d8a7c7141fc05b682a65fe0fdfcbb8f27d7c38be
DizzyHurricane5001 <72887223+DizzyHurricane5001@users.noreply.github.com>
2020-10-19 15:41:22 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,14 @@ public class Main {
     public static void main (String[] args) throws Exception {
         db = new SQLdb(); // initialize SQL JDBC
         db.connectDB();
-        db.insertData("serverData", 1, Tycoon.tycoonAPICall(1,"/status/widget/players.json"));
+        db.resetServerData();
+        boolean dbUpdate = true;
+        while (dbUpdate) {
+            for (int x = 0; x < 10; x++) {
+                db.insertData("serverData", x, Tycoon.tycoonAPICall(x,"/status/widget/players.json"));
+            }
+            Thread.sleep(10000); // PAGE/DB UPDATE EVERY 10 SECONDS. 
+        }
         db.close(); // end JDBC Connection
     }
 }
