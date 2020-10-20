0bb2e53ddba71463ebabf7160623367af9981420
DizzyHurricane5001 <72887223+DizzyHurricane5001@users.noreply.github.com>
2020-10-21 00:13:45 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,23 +3,19 @@ public class Main {
     public static SQLdb db;
 
     public static void main (String[] args) throws Exception {
-        db = new SQLdb(); // initialize SQL JDBC
+        db = new SQLdb(); 
         db.connectDB();
+        final int sleepTime = 120000;
         boolean dbUpdate = true;
-
-        db.insertData("flightData", 0, Tycoon.tycoonAPICall(0, "/status/airline.json"));
-      // Tycoon.tycoonAPICall(0, "/status/airline.json");
      while (dbUpdate) {
          db.resetServerData("flightdata");
          db.resetServerData("serverdata");
             for (int x = 0; x < 9; x++) {
                db.insertData("serverData", x, Tycoon.tycoonAPICall(x,"/status/widget/players.json"));
                 db.insertData("flightData", x, Tycoon.tycoonAPICall(x, "/status/airline.json"));
-                db.insertData("flightData", x, Tycoon.tycoonAPICall(x, "/status/airline.json"));
-                //Thread.sleep(100);
             }
-            Thread.sleep(120000); // PAGE UPDATE EVERY 10 SECONDS.
+            Thread.sleep(sleepTime); 
         }
-        db.close(); // end JDBC Connection
+        db.close(); 
     }
 }
