bd9925f2d3bedc709c6ef12e0a99caae6f142854
DizzyHurricane5001 <72887223+DizzyHurricane5001@users.noreply.github.com>
2020-10-21 00:09:07 +0100

    Update Tycoon.java

--- Tycoon.java
+++ Tycoon.java
@@ -17,45 +17,35 @@ import java.net.URLConnection;
 public class Tycoon {
     private static final String[] serverList =
             {"http://server.tycoon.community:30120",
-            "http://server.tycoon.community:30122",
-            "http://server.tycoon.community:30123",
-            "http://server.tycoon.community:30124",
-            "http://server.tycoon.community:30125",
-            "http://na.tycoon.community:30120",
-            "http://na.tycoon.community:30122",
-            "http://na.tycoon.community:30123",
-            "http://na.tycoon.community:30124",
-                    "http://na.tycoon.community:30124"};
+                    "http://server.tycoon.community:30122",
+                    "http://server.tycoon.community:30123",
+                    "http://server.tycoon.community:30124",
+                    "http://server.tycoon.community:30125",
+                    "http://na.tycoon.community:30122",
+                    "http://na.tycoon.community:30123",
+                    "http://na.tycoon.community:30124",
+                    "http://na.tycoon.community:30125"};
 
-    private static String TycoonAPIKey = "Tycoon_API_KEY"; // /api key new
+    private static String TycoonAPIKey = "TYCOON_API_KEY";
     private static URL url;
     private static URLConnection request;
 
-    public static String tycoonAPICall(int num, String directory) throws IOException { //0-8
+    public static String tycoonAPICall(int num, String directory) throws IOException {
         try {
-                url = new URL( serverList[num] + directory);
-                request = url.openConnection();
-                request.setConnectTimeout(500);
-                request.setRequestProperty("X-Tycoon-Key", TycoonAPIKey);
-                request.connect();
-                System.out.println("Did server number:" + num);
-                return pageReader(url);
+            String auth = TycoonAPIKey;
+            url = new URL(serverList[num] + directory);
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            HttpGet request = new HttpGet(serverList[num] + directory);
+            request.addHeader("X-Tycoon-Key", TycoonAPIKey);
+            CloseableHttpResponse response = httpClient.execute(request);
+            HttpEntity entity = response.getEntity();
+            if (entity != null) {
+                String result = EntityUtils.toString(entity);
+                return result;
+            }
         } catch (Exception e) {
-            System.out.println("error URL");
+            System.out.println("Server " + num + "refused the connection!" + e);
         }
-        return pageReader(url);
-    }
-
-
-    private static String pageReader(URL connection) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.openStream()));
-        String line;
-        while ((line = reader.readLine()) != null)
-        {
-           // System.out.println(line);
-            return line;
-        }
-        reader.close();
-        return "";
+        return null;
     }
 }
